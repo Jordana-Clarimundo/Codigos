285e1b5adb6ca1636b80fc634b7c8cee7a2b4815
Jordana Clarimundo Dionisio <115229693+Jordana-Clarimundo@users.noreply.github.com>
2023-05-11 15:13:12 -0300

    tarefa lista dart

--- tarefa lista.dart
+++ tarefa lista.dart
@@ -3,11 +3,11 @@ void main() {
   List<String> meusLivros = ['Talvez um dia', 'Assim que acaba', 'Uma segunda chance'];
 
   meusLivros.add('Todas as suas Imperfeições'); //adicionar
-
+print(meusLivros);
   meusLivros.remove('Talvez um dia'); //remover
-
+  print(meusLivros);
   
-  if (meusLivros.contains('Todas as suas Imperfeições')) { //pesquisar 
+  if (meusLivros.contains('Assim que acaba')) { //pesquisar 
     print('Contém esse livro na lista');
   } else {
     print("Não contém esse livro na lista"); 
